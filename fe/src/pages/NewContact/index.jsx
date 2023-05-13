7c75eccb74ef125be9c1eba70a0de5af689dcfb3
Alex Sander <alexsander01@hotmail.com.br>
2023-05-13 11:15:33 -0300

    feat: Created input and select in NewContact

--- fe/src/pages/NewContact/index.jsx
+++ fe/src/pages/NewContact/index.jsx
@@ -1,9 +1,22 @@
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select.js';
+import Input from '../../components/input';
 
 export default function NewContact() {
   return (
-    <PageHeader 
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader title="Novo Contato"/>
+        
+      <Input placeholder='Nome...'/>
+      <Select> 
+      <option value="">Selecione uma opção</option>
+      <option value="123">Instagram</option>
+      <option value="123">Facebook</option>
+      <option value="123">Twitter</option>
+      <option value="123">Email</option>
+
+
+      </Select>
+    </>
   );
 }
